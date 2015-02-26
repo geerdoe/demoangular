fabbaa3f5669721f788a255ad99cd0093534a9cb
Geer <german@mobaires.com>
2015-02-26 12:30:30 -0300

    first commit

---
+++ js/services.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* Services */
+
+/*
+ http://docs.angularjs.org/api/ngResource.$resource
+
+ Default ngResources are defined as
+
+ 'get':    {method:'GET'},
+ 'save':   {method:'POST'},
+ 'query':  {method:'GET', isArray:true},
+ 'remove': {method:'DELETE'},
+ 'delete': {method:'DELETE'}
+
+ */
+
+var services = angular.module('ngdemo.services', ['ngResource']);
+
+services.factory('DummyFactory', function ($resource) {
+    return $resource('http://demo7519446.mockable.io/userscrud/user-list', {}, {
+        query: { method: 'GET', params: {}, isArray: false }
+    })
+});
+
+services.factory('UsersFactory', function ($resource) {
+    return $resource('http://demo7519446.mockable.io/userscrud/user-list', {}, {
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+    })
+});
+
+services.factory('UserFactory', function ($resource) {
+    return $resource('http://demo7519446.mockable.io/userscrud/user-detail/:id', {}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT', params: {id: '@id'} },
+        delete: { method: 'DELETE', params: {id: '@id'} }
+    })
+});
